8738326cc965e7860943d948cc3076a779b801d8
Frank Utibe Joseph <majesticfrank7@gmail.com>
2024-10-31 21:55:23 +0100

    updated WelcomeMessage.jsx file

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import WelcomeMessage from './components/WelcomeMessage'
 import HeaderFile from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -38,6 +39,7 @@ function App() {
       <HeaderFile/>
       <MainContent/>
       <Footer/>
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography"/>
     </>
   )
 }
