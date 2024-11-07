ba7606ffb5517e34e3eecb873abda6a5ed8a0f9b
Frank Utibe Joseph <majesticfrank7@gmail.com>
2024-11-08 00:54:56 +0100

    added inline css to component

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer(){
+    return(
+        <div>
+ <footer style={{border:'solid blue 2px', backgroundColor:'whitesmoke', color:'blue', fontWeight:'700'}}>
+  <p>© 2023 City Lovers</p>
+</footer>
+ </div>
+    );
+}
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+function MainContent(){
+    return(
+        <div>
+<main>
+  <p> <span style={{color:'blue',letterSpacing:'2px'}}>I love to visit New York, </span>Paris, and Tokyo.</p>
+</main>
+</div>
+    );
+}
+export default MainContent;
