ab68c14add868a4fc6d47657c347c7178a62b7c3
Frank Utibe Joseph <majesticfrank7@gmail.com>
2024-11-08 00:58:08 +0100

    added inline css to component

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -2,7 +2,7 @@ function MainContent(){
     return(
         <div>
 <main style={{marginTop:'50px'}}>
-  <p> <span style={{color:'blue',letterSpacing:'2px'}}>I love to visit New York, </span>Paris, and Tokyo.</p>
+  <p> <span style={{color:'blue'}}>I love to visit New York,</span>Paris, and Tokyo.</p>
 </main>
 </div>
     );
