65e708f7f72e65c0b2d2613edc154360de30104d
Frank Utibe Joseph <majesticfrank7@gmail.com>
2024-11-17 03:48:41 +0100

    used state management and cusftom hooks

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react"
-import { RecipeList } from "./components/RecipeList"
-import { AddRecipeForm } from "./components/AddRecipeForm"
+import  AddRecipeForm  from "./components/AddRecipeForm"
+import RecipeList from "./components/RecipeList"
+
 
 function App() {
  
