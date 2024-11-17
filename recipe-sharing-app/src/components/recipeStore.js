75ab5b88e47653212ba28cbeeffc444c4de412c5
Frank Utibe Joseph <majesticfrank7@gmail.com>
2024-11-17 04:14:24 +0100

    used state management and cusftom hooks

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,10 +1,7 @@
-import {create} from 'zustand'
+import create from 'zustand'
 
-  export const useRecipeStore = create((set) => ({
-    recipes : [],
-    addRecipe: (newRecipe) => set((state)=>({recipes: [...state.recipes,newRecipe]})),
-    removeRecipe:(newRecipeId)=>set((state)=> ({recipes: state.recipes.filter(newRecipe => newRecipe.id !== newRecipeId)})),
-    clearRecipe:() => set({recipes: []})
-
-
- }));
+const useRecipeStore = create(set => ({
+  recipes: [],
+  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
+  setRecipes: (recipes) => set({ recipes })
+}));
