d036c3408c33800bbffab3804070c49536435eca
Frank Utibe Joseph <majesticfrank7@gmail.com>
2024-11-23 18:24:48 +0100

    updated

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 
-import Registrationform from './Components/Registrationform'
+import Registrationform from './components/Registrationform'
+
 function App() {
 
 
@@ -8,6 +9,7 @@ function App() {
 <div>
   
 <Registrationform />
+<formikForm/>
 
 
 </div>
