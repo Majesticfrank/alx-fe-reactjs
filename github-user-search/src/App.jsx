182918a1cf6679f4e03ca21779f44a22c67b9023
Frank utibe joseph <majesticfrank7@gmail.com>
2024-12-15 23:31:43 +0100

    implemented logic for searching gith hub username

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Search from './components/search'
+
+function App() {
+
+  return (
+    
+      <div>
+        <h1>GitHub User Search</h1>
+        <Search />
+        
+    </div>
+  )
+}
+
+export default App
---
+++ github-user-search/src/components/Search.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { fetchUserData } from '../services/githubService';
+
+const Search = () => {
+  const [username, setUsername] = useState("");
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const handleInputChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setError(false);
+    setUserData(null);
+
+    try {
+      const data = await fetchUserData(username);
+      setUserData(data);
+    } catch (err) {
+      setError(true);
+    } finally {
+      setLoading(false);
+      setUsername('')
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={username}
+          onChange={handleInputChange}
+          placeholder="Enter GitHub username"
+        />
+        <button type="submit">Search</button>
+      </form>
+
+      {loading && <p>Loading...</p>}
+      {error && <p>Looks like we can't find the user.</p>}
+      
+      {userData && (
+        <div>
+          <h3>{userData.name}</h3>
+          <img src={userData.avatar_url} alt={`${userData.name} avatar`} width={100} />
+          <p>{userData.bio}</p>
+          <a href={userData.html_url} target="_blank" rel="noopener noreferrer">Visit GitHub Profile</a>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Search;
+
